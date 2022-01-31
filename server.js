507c3841f9dfbb548dd5354293df7198e311fb41
Aaron Bean <arbean1978@gmail.com>
2022-01-31 11:07:23 -0700

    again

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your mysql username
+        user: 'root',
+        //your mysql password
+        password: 'Laceybabe#10213!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
 
 //default response for any other request (Not Found)
 app.use((req,res) => {
